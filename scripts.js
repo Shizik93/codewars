943101ae7a700d8232060c072e80c69bc3677bd6
AleksandrKayuda <akayuda999@gmail.com>
2022-09-08 18:18:30 +0300

    There is an array with some numbers. All numbers are equal except for one. Try to find it!
    findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
    findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
    It’s guaranteed that array contains at least 3 numbers.
    
    The tests contain some very huge arrays, so think about performance.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
+function findUniq(arr) {
+  arr.sort((a, b) => a - b)
+  const first = arr.filter(el => el === arr[0])
+  const last = arr.filter(el => el === arr[arr.length - 1])
+  if (first.length > 1) {
+    return last[0]
+  } else {
+    return first[0]
+  }
+}
 
+
+console.log(findUniq([0, 1, 0]))
+console.log(findUniq([0, 0, 1]))
+console.log(findUniq([1, 1, 1, 2, 1, 1]))
+console.log(findUniq([1, 1, 2, 1, 1]))
+console.log(findUniq([3, 10, 3, 3, 3]))
