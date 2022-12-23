219dff504b93be5304111120e3ee2a5e6d044c85
AleksandrKayuda <akayuda999@gmail.com>
2022-12-23 18:54:39 +0300

    Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
    Example
    createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
    The returned format must be correct in order to complete this challenge.
    
    Don't forget the space after the closing parentheses!
    arrayDiff([1,2],[1]) == [2]
    If a value is present in b, all of its occurrences must be removed from the other:
    
    arrayDiff([1,2,2,2,3],[2]) == [1,3]

---
+++ scripts.js
@@ -0,0 +1,116 @@
+/*
+const pow = (n) => {
+if(n===0){
+    return 1
+}
+return n*pow(n-1)
+}
+console.log(pow(5))*/
+
+/*
+let list = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
+        }
+    }
+};
+
+const listReader=(list)=>{
+    console.log(list.value)
+    if(list.next){
+       listReader(list.next)
+    }
+
+}
+listReader(list)*/
+
+
+/*let list = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
+        }
+    }
+};
+
+const listReader = (list) => {
+    if(list.next){
+        listReader(list.next)
+    }
+    console.log(list.value)
+
+
+}
+listReader(list)*/
+
+/*
+
+const factorial = (n) => {
+    debugger
+    if (n === 1) {
+        return 1
+    }
+    return n * factorial((n - 1))
+}
+console.log(factorial(5))*/
+
+
+/*
+const sumTo=(n)=>{
+    debugger
+    if(n===1){
+        return 1
+    }
+    return n+sumTo(n-1)
+
+}
+console.log(sumTo(4))*/
+/*
+const o = {
+    a: 7,
+    get b() {
+        return this.a + 1;
+    },
+    set c(x) {
+        this.a = x / 2;
+    }
+};
+
+const newO=JSON.stringify(o)
+const aaa=JSON.parse(newO)
+console.dir(o)
+console.dir(aaa)*/
+
+
+
+/*const obj = {
+    a: 7,
+    b: '12',
+    c: 2323,
+    d: [1, 2, 3, 4],
+    e: {
+        ab: 1,
+        bc: 'sdsd'
+    },
+    f: function (x) {
+        return x + x
+    }
+}
+
+const newObj = JSON.stringify(obj)
+console.dir(obj)
+console.log(newObj)
+console.dir(JSON.parse(newObj))*/
